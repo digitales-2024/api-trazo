c3ee8a1a1b612a39404901d92851c2443af96069
Diego Rondón <furrtr4532@gmail.com>
2024-10-24 09:08:20 -0500

    feat: agregar controlador de clientes

--- src/modules/admin/clients/clients.controller.ts
+++ src/modules/admin/clients/clients.controller.ts
@@ -1,15 +1,38 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { ClientsService } from './clients.service';
 import { CreateClientDto } from './dto/create-client.dto';
 import { UpdateClientDto } from './dto/update-client.dto';
+import { Auth, GetUser } from '@login/login/admin/auth/decorators';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { UserData } from '@login/login/interfaces';
 
-@Controller('clients')
+@ApiTags('Client')
+@ApiBadRequestResponse({ description: 'Bad Request' })
+@ApiUnauthorizedResponse({ description: 'Unauthorized' })
+@Auth()
+@Controller({ path: 'clients', version: '1' })
 export class ClientsController {
   constructor(private readonly clientsService: ClientsService) {}
 
+  @ApiCreatedResponse({
+    description: 'Client successfully created',
+  })
   @Post()
-  create(@Body() createClientDto: CreateClientDto) {
-    return this.clientsService.create(createClientDto);
+  create(@Body() createClientDto: CreateClientDto, @GetUser() user: UserData) {
+    return this.clientsService.create(createClientDto, user);
   }
 
   @Get()
