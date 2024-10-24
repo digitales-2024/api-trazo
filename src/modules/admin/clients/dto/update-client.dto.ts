942ff4ebe485d15b3d0388104ff44171122ea325
Diego Rondón <furrtr4532@gmail.com>
2024-10-24 12:21:00 -0500

    feat: agregar validación de longitud para el campo name en UpdateClientDto

--- src/modules/admin/clients/dto/update-client.dto.ts
+++ src/modules/admin/clients/dto/update-client.dto.ts
@@ -1,4 +1,21 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateClientDto } from './create-client.dto';
+import { IsNotEmpty, IsString } from 'class-validator';
+import { Transform } from 'class-transformer';
 
-export class UpdateClientDto extends PartialType(CreateClientDto) {}
+export class UpdateClientDto extends PartialType(CreateClientDto) {
+  @ApiProperty({
+    name: 'name',
+    description: 'Client name',
+    required: false,
+  })
+  @IsString()
+  @IsNotEmpty()
+  @Transform(({ value }) => value.trim().toLowerCase())
+  name?: string;
+  rucDni?: string;
+  address?: string;
+  phone?: string;
+  province?: string;
+  department?: string;
+}
