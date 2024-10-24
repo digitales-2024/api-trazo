ae2898933dda607165489a659cd37a63527c828d
Alx <worfysito15@gmail.com>
2024-10-24 15:56:55 -0500

    feat: agregar clase CreateSpaceDto para la creación de espacios

---
+++ libs/design-projects/src/spaces/dto/create-space.dto.ts
@@ -0,0 +1,23 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateSpaceDto {
+  @ApiProperty({
+    name: 'name',
+    description: 'Space name',
+  })
+  @IsString()
+  @IsNotEmpty()
+  @Transform(({ value }) => value.trim().toLowerCase())
+  name: string;
+
+  @ApiProperty({
+    name: 'name',
+    description: 'Description',
+  })
+  @IsString()
+  @IsNotEmpty()
+  @Transform(({ value }) => value.trim().toLowerCase())
+  description: string;
+}
