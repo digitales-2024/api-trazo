287a211d081a480d064bf9df38b2e52096511c53
Diego Rondón <furrtr4532@gmail.com>
2024-10-24 09:25:09 -0500

    feat: agregar funcionalidad de creación de clientes

--- src/modules/admin/clients/clients.service.ts
+++ src/modules/admin/clients/clients.service.ts
@@ -1,11 +1,106 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  HttpStatus,
+  Injectable,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateClientDto } from './dto/create-client.dto';
 import { UpdateClientDto } from './dto/update-client.dto';
+import { HttpResponse, UserData } from '@login/login/interfaces';
+import { PrismaService } from '@prisma/prisma';
+import { handleException } from '@login/login/utils';
+import { ClientData } from 'src/interfaces';
+import { AuditActionType } from '@prisma/client';
 
 @Injectable()
 export class ClientsService {
-  create(createClientDto: CreateClientDto) {
-    return 'This action adds a new client';
+  private readonly logger = new Logger(ClientsService.name);
+  constructor(private readonly prisma: PrismaService) {}
+
+  /**
+   * Crear un nuevo cliente
+   * @param createClientDto Dto con los datos del cliente a crear
+   * @param user Usuario que realiza la acción
+   * @returns Nuevo cliente creado
+   */
+  async create(
+    createClientDto: CreateClientDto,
+    user: UserData,
+  ): Promise<HttpResponse<ClientData>> {
+    const { name, address, phone, rucDni, province, department } =
+      createClientDto;
+    let newClient;
+
+    try {
+      // Crear el cliente y registrar la auditoría
+      newClient = await this.prisma.$transaction(async () => {
+        // Crear el nuevo cliente
+        const client = await this.prisma.client.create({
+          data: {
+            name,
+            address,
+            phone,
+            rucDni,
+            province,
+            department,
+          },
+          select: {
+            id: true,
+            name: true,
+            address: true,
+            phone: true,
+            rucDni: true,
+            province: true,
+            department: true,
+            isActive: true,
+          },
+        });
+
+        // Registrar la auditoría de la creación del cliente
+        await this.prisma.audit.create({
+          data: {
+            action: AuditActionType.CREATE,
+            entityId: client.id,
+            entityType: 'client',
+            performedById: user.id,
+          },
+        });
+
+        return client;
+      });
+
+      return {
+        statusCode: HttpStatus.CREATED,
+        message: 'Client created successfully',
+        data: {
+          id: newClient.id,
+          name: newClient.name,
+          rucDni: newClient.rucDni,
+          phone: newClient.phone,
+          address: newClient.address,
+          province: newClient.province,
+          department: newClient.department,
+          isActive: newClient.isActive,
+        },
+      };
+    } catch (error) {
+      this.logger.error(`Error creating client: ${error.message}`, error.stack);
+
+      if (newClient) {
+        await this.prisma.client.delete({ where: { id: newClient.id } });
+        this.logger.error(`Client has been deleted due to error in creation.`);
+      }
+
+      if (
+        error instanceof BadRequestException ||
+        error instanceof NotFoundException
+      ) {
+        throw error;
+      }
+
+      handleException(error, 'Error creating a client');
+    }
   }
 
   findAll() {
