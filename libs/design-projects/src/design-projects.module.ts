2507c4e5ec4f06deb316835e31854c275877e66e
Fernando Araoz <fernando@araozu.dev>
2024-10-25 09:50:26 -0500

    feat: crear schema Quotation y estructura de archivos para su CRUD

--- libs/design-projects/src/design-projects.module.ts
+++ libs/design-projects/src/design-projects.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DesignProjectsService } from './design-projects.service';
 import { SpacesModule } from './spaces/spaces.module';
+import { QuotationsModule } from './quotations/quotations.module';
 
 @Module({
   providers: [DesignProjectsService],
   exports: [DesignProjectsService, DesignProjectsModule],
-  imports: [SpacesModule],
+  imports: [SpacesModule, QuotationsModule],
 })
 export class DesignProjectsModule {}
