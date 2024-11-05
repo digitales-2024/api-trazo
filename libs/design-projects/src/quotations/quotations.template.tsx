a961a4ca6b661308709bd80ca27d9daa07dfd790
Fernando Araoz <fernando@araozu.dev>
2024-11-05 13:09:05 -0500

    fix(pdf): manejar posibles errores al cargar estilos de tailwind

--- libs/design-projects/src/quotations/quotations.template.tsx
+++ libs/design-projects/src/quotations/quotations.template.tsx
@@ -15,10 +15,20 @@ export class QuotationTemplate {
    * @param param0 An object with children to render inside the skeleton
    */
   private static Skeleton({ children }: { children: JSX.Element }) {
-    // TODO: On production, compile and include the css file only once
-    const tailwindFile = Fs.readFileSync(
-      Path.join(process.cwd(), 'static', 'tailwind-output.css'),
-    ).toString();
+    // Loads the tailwind output to use in the pdf rendering.
+    // Last I measured, this file had a size of 6.8KiB.
+    // Since we don't expect PDF generation to be a feature used
+    // constantly, we rather incur a speed penalty loading the file
+    // over and over rather than using 6.8KiB additional RAM.
+    let tailwindFile = '';
+    try {
+      tailwindFile = Fs.readFileSync(
+        Path.join(process.cwd(), 'static', 'tailwind-output.css'),
+      ).toString();
+    } catch (e) {
+      console.error('Error loading tailwind file:');
+      console.error(e);
+    }
 
     return (
       <>
