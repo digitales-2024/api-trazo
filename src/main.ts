84ad55ab4cab2275bfc801b84066e4b9d2b33261
Fernando Araoz <fernando@araozu.dev>
2024-11-05 13:14:05 -0500

    fix: desactivar endpoint de swagger y CORS en produccion

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,14 @@ import { apiReference } from '@scalar/nestjs-api-reference';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: process.env.WEB_URL,
-    credentials: true,
-  });
+
+  if (process.env.NODE_ENV !== 'production') {
+    app.enableCors({
+      origin: process.env.WEB_URL,
+      credentials: true,
+    });
+  }
+
   app.use(cookieParser());
 
   app.enableVersioning({
@@ -26,36 +30,38 @@ async function bootstrap() {
     }),
   );
 
-  const config = new DocumentBuilder()
-    .setTitle('Trazo API')
-    .setDescription('API for Trazo')
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
+  if (process.env.NODE_ENV !== 'production') {
+    const config = new DocumentBuilder()
+      .setTitle('Trazo API')
+      .setDescription('API for Trazo')
+      .setVersion('1.0')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
 
-  document.tags = [
-    { name: 'Auth', description: 'Operations about authentication' },
-    { name: 'Admin', description: 'Operations about admin' },
-    { name: 'Users', description: 'Operations about users' },
-    { name: 'Rol', description: 'Operations about roles' },
-    { name: 'Modules', description: 'Operations about modules' },
-    { name: 'Permissions', description: 'Operations about permissions' },
-    { name: 'Audit', description: 'Operations about audit' },
-    { name: 'Business', description: 'Operations about the business' },
-    { name: 'Quotation', description: 'Operations about quotations' },
-    { name: 'Levels', description: 'Operations about levels' },
-  ];
+    document.tags = [
+      { name: 'Auth', description: 'Operations about authentication' },
+      { name: 'Admin', description: 'Operations about admin' },
+      { name: 'Users', description: 'Operations about users' },
+      { name: 'Rol', description: 'Operations about roles' },
+      { name: 'Modules', description: 'Operations about modules' },
+      { name: 'Permissions', description: 'Operations about permissions' },
+      { name: 'Audit', description: 'Operations about audit' },
+      { name: 'Business', description: 'Operations about the business' },
+      { name: 'Quotation', description: 'Operations about quotations' },
+      { name: 'Levels', description: 'Operations about levels' },
+    ];
 
-  SwaggerModule.setup('api', app, document);
+    SwaggerModule.setup('api', app, document);
 
-  app.use(
-    '/reference',
-    apiReference({
-      spec: {
-        content: document,
-      },
-    }),
-  );
+    app.use(
+      '/reference',
+      apiReference({
+        spec: {
+          content: document,
+        },
+      }),
+    );
+  }
 
   await app.listen(process.env.PORT ?? 3000);
 }
