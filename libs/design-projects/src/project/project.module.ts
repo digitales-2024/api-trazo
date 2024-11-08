1c7dad7e8827165c0d65e3a74c59eed2f332e07f
_JMNRA <jmedinan@unsa.edu.pe>
2024-11-08 17:30:53 -0500

    feat(project): importaciones para realizar validaciones

--- libs/design-projects/src/project/project.module.ts
+++ libs/design-projects/src/project/project.module.ts
@@ -5,10 +5,17 @@ import { PrismaModule } from '@prisma/prisma';
 import { AuditModule } from '@login/login/admin/audit/audit.module';
 import { ClientsModule } from '@clients/clients';
 import { UsersModule } from '@login/login/admin/users/users.module';
+import { QuotationsModule } from '../quotations/quotations.module';
 
 @Module({
   controllers: [ProjectController],
   providers: [ProjectService],
-  imports: [PrismaModule, AuditModule, ClientsModule, UsersModule],
+  imports: [
+    PrismaModule,
+    AuditModule,
+    ClientsModule,
+    UsersModule,
+    QuotationsModule,
+  ],
 })
 export class ProjectModule {}
