e543dfa57ddf930ca760b0199191c383f7088a5f
_JMNRA <jmedinan@unsa.edu.pe>
2024-11-11 11:53:19 -0500

    refactor(prisma): eliminar campo meetings en DesignProject

---
+++ prisma/migrations/20241111165201_remove_meetings_design_project_model/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `meetings` on the `DesignProject` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "DesignProject" DROP COLUMN "meetings";
