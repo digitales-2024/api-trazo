bb597b69fac07df66b6f48e86cc0c170313c3ec1
Diego Rondón <furrtr4532@gmail.com>
2024-11-12 10:22:04 -0500

    feat(interfaces): actualizar la interfaz 'ZoningData'

--- libs/design-projects/src/interfaces/zoning.interfaces.ts
+++ libs/design-projects/src/interfaces/zoning.interfaces.ts
@@ -2,10 +2,5 @@ import { Zoning } from '@prisma/client';
 
 export type ZoningData = Pick<
   Zoning,
-  | 'id'
-  | 'zone_code'
-  | 'description'
-  | 'buildable_area'
-  | 'open_area'
-  | 'isActive'
+  'id' | 'zoneCode' | 'description' | 'buildableArea' | 'openArea' | 'isActive'
 >;
