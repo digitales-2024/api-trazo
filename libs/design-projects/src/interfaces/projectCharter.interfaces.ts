576d5be249009823618edc4151225846d5ab3440
Diego Rondón <furrtr4532@gmail.com>
2024-11-15 17:24:49 -0500

    feat(projectCharter): agregar interfaz ProjectCharterData

---
+++ libs/design-projects/src/interfaces/projectCharter.interfaces.ts
@@ -0,0 +1,17 @@
+import { DesignProjectStatus, ProjectCharter } from '@prisma/client';
+
+export type ProjectCharterData = Pick<ProjectCharter, 'id'> & {
+  designProject: {
+    id: string;
+    code: string;
+    status: DesignProjectStatus;
+    client: {
+      id: string;
+      name: string;
+    };
+    designer: {
+      id: string;
+      name: string;
+    };
+  };
+};
