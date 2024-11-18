0311463e859aa24237b741e276c2aabb579c7e08
_JMNRA <jmedinan@unsa.edu.pe>
2024-11-18 12:28:24 -0500

    refactor:  eliminar tipos no usados

--- libs/project-execution/src/interfaces/resource.interfaces.ts
+++ libs/project-execution/src/interfaces/resource.interfaces.ts
@@ -5,12 +5,3 @@ export type ResourceData = Pick<
   Resource,
   'id' | 'type' | 'name' | 'unit' | 'unitCost' | 'isActive'
 >;
-
-// Para listados resumidos si se necesitan
-export type ResourceSummaryData = Pick<
-  Resource,
-  'id' | 'type' | 'name' | 'isActive'
->;
-
-// Para datos anidados si se necesitan
-export type ResourceDataNested = Omit<Resource, 'createdAt' | 'updatedAt'>;
