d3df00654c2df78e5a965517f3fd5fa03deb2ced
Diego Rondón <furrtr4532@gmail.com>
2024-11-22 11:14:49 -0500

    feat: agregar BudgetModule al módulo ProjectExecution

--- libs/project-execution/src/project-execution.module.ts
+++ libs/project-execution/src/project-execution.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ProjectExecutionService } from './project-execution.service';
 import { ResourceModule } from './resource/resource.module';
+import { BudgetModule } from './budget/budget.module';
 
 @Module({
   providers: [ProjectExecutionService],
   exports: [ProjectExecutionService],
-  imports: [ResourceModule],
+  imports: [ResourceModule, BudgetModule],
 })
 export class ProjectExecutionModule {}
