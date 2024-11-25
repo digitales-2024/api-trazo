09b126c6f1d0e103a6aa2c75dc7692efa15429cc
Diego Rondón <furrtr4532@gmail.com>
2024-11-25 14:51:02 -0500

    feat: agregar propiedad category a SubcategoryData para incluir información de la categoría

--- libs/project-execution/src/interfaces/subcategory.interfaces.ts
+++ libs/project-execution/src/interfaces/subcategory.interfaces.ts
@@ -1,3 +1,5 @@
 import { Subcategory } from '@prisma/client';
 
-export type SubcategoryData = Pick<Subcategory, 'id' | 'name' | 'isActive'>;
+export type SubcategoryData = Pick<Subcategory, 'id' | 'name' | 'isActive'> & {
+  category: { id: string; name: string };
+};
