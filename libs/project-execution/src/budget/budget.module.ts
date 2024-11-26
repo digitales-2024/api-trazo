0f50a20f0f8a73caad8305b8780d5540d64b0d0f
Diego Rondón <furrtr4532@gmail.com>
2024-11-26 10:32:08 -0500

    feat: agregar módulos Clients y Project al módulo Budget

--- libs/project-execution/src/budget/budget.module.ts
+++ libs/project-execution/src/budget/budget.module.ts
@@ -3,10 +3,12 @@ import { BudgetService } from './budget.service';
 import { BudgetController } from './budget.controller';
 import { PrismaModule } from '@prisma/prisma';
 import { QuotationsModule } from '@design-projects/design-projects/quotations/quotations.module';
+import { ClientsModule } from '@clients/clients';
+import { ProjectModule } from '@design-projects/design-projects/project/project.module';
 
 @Module({
   controllers: [BudgetController],
   providers: [BudgetService],
-  imports: [PrismaModule, QuotationsModule],
+  imports: [PrismaModule, QuotationsModule, ClientsModule, ProjectModule],
 })
 export class BudgetModule {}
