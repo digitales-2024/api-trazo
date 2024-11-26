3e08ab8c0048157e47916b23de937bc7ca07b6a7
Diego Rondón <furrtr4532@gmail.com>
2024-11-26 12:56:19 -0500

    feat: agregar módulos de categoría y subcategoría al módulo de presupuesto

--- libs/project-execution/src/budget/budget.module.ts
+++ libs/project-execution/src/budget/budget.module.ts
@@ -5,10 +5,19 @@ import { PrismaModule } from '@prisma/prisma';
 import { QuotationsModule } from '@design-projects/design-projects/quotations/quotations.module';
 import { ClientsModule } from '@clients/clients';
 import { ProjectModule } from '@design-projects/design-projects/project/project.module';
+import { CategoryModule } from '../category/category.module';
+import { SubcategoryModule } from '../subcategory/subcategory.module';
 
 @Module({
   controllers: [BudgetController],
   providers: [BudgetService],
-  imports: [PrismaModule, QuotationsModule, ClientsModule, ProjectModule],
+  imports: [
+    PrismaModule,
+    QuotationsModule,
+    ClientsModule,
+    ProjectModule,
+    CategoryModule,
+    SubcategoryModule,
+  ],
 })
 export class BudgetModule {}
