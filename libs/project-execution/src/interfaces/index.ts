ef69980e7425a1bd7ab8f8d1d403e44ffdc5dfe7
Diego Rondón <furrtr4532@gmail.com>
2024-11-27 11:23:52 -0500

    feat: agregar CategoryBudgetDetails a las exportaciones en index.ts

--- libs/project-execution/src/interfaces/index.ts
+++ libs/project-execution/src/interfaces/index.ts
@@ -1,4 +1,8 @@
 export { ResourceData } from './resource.interfaces';
 export { CategoryData } from './category.interfaces';
 export { SubcategoryData } from './subcategory.interfaces';
-export { BudgetData, SummaryBudgetData } from './budget.interfaces';
+export {
+  BudgetData,
+  CategoryBudgetDetails,
+  SummaryBudgetData,
+} from './budget.interfaces';
