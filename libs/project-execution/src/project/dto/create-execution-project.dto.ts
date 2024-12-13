93515bf132ff8ae0f35724c18d5d35e2c376d13c
victorsalazar <victormsi.x20@gmail.com>
2024-12-13 09:46:16 -0500

    feat: creacion de proyecto de ejecucion

---
+++ libs/project-execution/src/project/dto/create-execution-project.dto.ts
@@ -0,0 +1,66 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
+import { IsNotEmpty, IsUUID, IsString, IsDateString } from 'class-validator';
+
+export class CreateExecutionProjectDto {
+  @ApiProperty({
+    description: 'Name of the project',
+  })
+  @IsString()
+  @IsNotEmpty()
+  @Transform(({ value }) => value.trim())
+  name: string;
+
+  @ApiProperty({
+    description: 'Location of the project',
+    example: 'Lima, Peru',
+  })
+  @IsString()
+  @IsNotEmpty()
+  ubicationProject: string;
+
+  @ApiProperty({
+    name: 'province',
+    description: 'Client province',
+  })
+  @IsString()
+  @IsNotEmpty()
+  @Transform(({ value }) => value.trim())
+  province: string;
+
+  @ApiProperty({
+    name: 'department',
+    description: 'Client department',
+  })
+  @IsString()
+  @IsNotEmpty()
+  department: string;
+
+  @ApiProperty({
+    description: 'Client ID associated with the project',
+  })
+  @IsUUID()
+  @IsNotEmpty()
+  clientId: string;
+
+  @ApiProperty({
+    description: 'Budget ID associated with the project',
+  })
+  @IsUUID()
+  @IsNotEmpty()
+  budgetId: string;
+
+  @ApiProperty({
+    description: 'Resident ID assigned to the project',
+  })
+  @IsUUID()
+  @IsNotEmpty()
+  residentId: string;
+
+  @ApiProperty({
+    description: 'Fecha para el inicio del proyecto',
+    example: '2024-01-15',
+  })
+  @IsDateString()
+  startProjectDate: string;
+}
