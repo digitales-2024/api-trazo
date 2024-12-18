6a326aaf8f42f7f3c43f8c034d91ffaa9e0aacf7
Diego Rondón <furrtr4532@gmail.com>
2024-12-18 11:19:21 -0500

    feat: agregar módulo Supplier en PurchaseOrderModule

--- libs/project-execution/src/purchase-order/purchase-order.module.ts
+++ libs/project-execution/src/purchase-order/purchase-order.module.ts
@@ -3,10 +3,11 @@ import { PurchaseOrderService } from './purchase-order.service';
 import { PurchaseOrderController } from './purchase-order.controller';
 import { PrismaModule } from '@prisma/prisma';
 import { ResourceModule } from '../resource/resource.module';
+import { SupplierModule } from '../supplier/supplier.module';
 
 @Module({
   controllers: [PurchaseOrderController],
   providers: [PurchaseOrderService],
-  imports: [PrismaModule, ResourceModule],
+  imports: [PrismaModule, ResourceModule, SupplierModule],
 })
 export class PurchaseOrderModule {}
