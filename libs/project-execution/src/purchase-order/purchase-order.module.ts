31969758d14a26facb68c6c56a7fdbc80e945f3d
Diego Rondón <furrtr4532@gmail.com>
2024-12-18 10:09:50 -0500

    feat: agregar ResourceModule a las importaciones del módulo PurchaseOrder

--- libs/project-execution/src/purchase-order/purchase-order.module.ts
+++ libs/project-execution/src/purchase-order/purchase-order.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { PurchaseOrderService } from './purchase-order.service';
 import { PurchaseOrderController } from './purchase-order.controller';
 import { PrismaModule } from '@prisma/prisma';
+import { ResourceModule } from '../resource/resource.module';
 
 @Module({
   controllers: [PurchaseOrderController],
   providers: [PurchaseOrderService],
-  imports: [PrismaModule],
+  imports: [PrismaModule, ResourceModule],
 })
 export class PurchaseOrderModule {}
