3221d1ac5c7cd33e1596be51f2b2eff0d21e8f28
Diego Rondón <furrtr4532@gmail.com>
2024-12-18 10:22:42 -0500

    feat: agregar SupplierData y SupplierDescriptionData a las interfaces del módulo ProjectExecution

--- libs/project-execution/src/interfaces/index.ts
+++ libs/project-execution/src/interfaces/index.ts
@@ -10,3 +10,4 @@ export { WorkItemData } from './workitem.interfaces';
 export { type FullApuBudgetData } from './apuBudget.interfaces';
 export type { SubWorkItemData } from './subWorkItem.interfaces';
 export { PurchaseOrderData } from './purchaseOrder.interfaces';
+export { SupplierData, SupplierDescriptionData } from './supplier.interfaces';
