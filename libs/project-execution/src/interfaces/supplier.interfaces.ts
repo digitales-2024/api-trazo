c8fd37357456c60b6e384e8e051da0745fa14544
Diego Rondón <furrtr4532@gmail.com>
2024-12-21 11:46:26 -0500

    feat: agregar campos 'department' y 'province' a las interfaces de 'Supplier'

--- libs/project-execution/src/interfaces/supplier.interfaces.ts
+++ libs/project-execution/src/interfaces/supplier.interfaces.ts
@@ -2,12 +2,28 @@ import { Supplier } from '@prisma/client';
 
 export type SupplierData = Pick<
   Supplier,
-  'id' | 'name' | 'ruc' | 'address' | 'phone' | 'email' | 'isActive'
+  | 'id'
+  | 'name'
+  | 'ruc'
+  | 'address'
+  | 'phone'
+  | 'email'
+  | 'department'
+  | 'province'
+  | 'isActive'
 >;
 
 export type SupplierDescriptionData = Pick<
   Supplier,
-  'id' | 'name' | 'ruc' | 'address' | 'phone' | 'email' | 'isActive'
+  | 'id'
+  | 'name'
+  | 'ruc'
+  | 'address'
+  | 'phone'
+  | 'email'
+  | 'department'
+  | 'province'
+  | 'isActive'
 > & {
   description: SupplierData[];
 };
